1aca8364cb74a3b8718c818f0fec775870e87ff6
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2017-07-22 09:36:15 -0700

    moved examples to sub folder

---
+++ nutmeg/examples/example_intra.py
@@ -0,0 +1,26 @@
+
+import os
+
+import IPython.display
+
+from processor import NutmegProbe, NutmegClip
+
+
+def example():
+    f = '/home/pierre/Videos/GoPro/Malibu/GOPR6248.MP4'
+
+    p = NutmegProbe()
+    p.probe(f)
+
+    print('\nContainer:')
+    IPython.display.display(p.results.container)
+
+    for s in p.results.streams:
+        print('\nStream {}:'.format(s.index))
+        IPython.display.display(s)
+
+
+#------------------------------------------------
+
+if __name__ == '__main__':
+    example()
